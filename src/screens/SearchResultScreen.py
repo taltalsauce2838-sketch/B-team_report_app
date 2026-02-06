87bab58a3e5d5262caca0b873c53a20505a34913
kazuki-debug <4a.lsxilo@gmail.com>
2026-02-06 11:18:21 +0900

    検索ボタン作成

--- src/screens/SearchResultScreen.py
+++ src/screens/SearchResultScreen.py
@@ -6,9 +6,13 @@ class SearchResultScreen(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         tk.Label(self, text="検索結果").pack()
+        tk.Button(self,text="検索",
+                  command=lambda: master.show_frame("searchScreen")).pack()
         tk.Button(self, text="詳細",
                   command=lambda: master.show_frame("DetailScreen")).pack(expand=True)
+
         #
         result = self.master.search_result
 
-        
+#    def list_task(connection):
+
