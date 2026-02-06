2d163730f85c51c302a2c637bf46140b085ad686
neko166 <126163783+neko166@users.noreply.github.com>
2026-02-06 11:00:10 +0900

    入力フォームを追加
    入力フォームと遷移のボタンを作りました。
    関数は実装できていないのでどのボタンを押しても検索に遷移するようになっています

--- src/screens/NewRegistrationIncident.py
+++ src/screens/NewRegistrationIncident.py
@@ -6,5 +6,32 @@ class NewRegistrationIncident(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         tk.Label(self, text="新規登録 Incident").pack()
+
+        
+        # 1. 項目名の定義（データ）
+        fields = ["種別", "タイトル", "背景", "現状", "次アクション", "注意点", "参考リンク"]
+        
+        # 2. 内容を保持するための変数（辞書）
+        self.entries = {}
+
+        # 3. ループによる一括生成
+        for field in fields:
+            # ラベルの配置
+            tk.Label(self, text=field).pack(anchor="w", padx=10)
+            
+            # Entryの生成と辞書への格納
+            entry = tk.Entry(self)
+            entry.pack(fill="x", padx=10, pady=(0, 5))
+            
+            # 項目名をキーにしてインスタンスを記憶
+            self.entries[field] = entry
+
+
+       #検索に遷移するボタン
         tk.Button(self, text="検索",
-                  command=lambda: master.show_frame("SearchScreen")).pack(expand=True)
+                  command=lambda: master.show_frame("SearchScreen")).place(relx=1.0, rely=0.0,anchor="ne",x=-10,y=10)
+
+        #登録を実行するボタン
+        tk.Button(self, text="登録",
+                  command=lambda: master.show_frame("SearchScreen")).place(relx=1.0, rely=1.0,anchor="se",x=-10,y=-10)
+        #中身が大嘘なので修正
